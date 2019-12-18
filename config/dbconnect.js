d10de39d2db130ad8afd3411802e7a385d5c46eb
Sibendu Sarker <sibendu.sarker@mitt.ca>
2019-12-18 15:48:23 -0600

    Datbase connection warning fixed.

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -6,7 +6,7 @@ const db = config.get('databaseURI');
 const connectDB = async () => {
 
 try {
-    await mongoose.connect(db);
+    await mongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true });
     console.log('Datbase connected .....')
 } catch (error) {
     
