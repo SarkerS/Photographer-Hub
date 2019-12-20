eaaeb9031cddc14910d944a5bdce3564f11d5316
Sibendu Sarker <sibendu.sarker@mitt.ca>
2019-12-20 13:49:59 -0600

    routes added

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ connectDB();
 
 app.get('/', (req, res)=> res.send('API Running'));
 
+//Routes defined
+app.use('/api/users', require('./routes/api/users'));
+app.use('api/userpost', require('./routes/api/userpost'));
+app.use('/api/authentication', require('./routes/api/authentication'));
+app.use('/api/userprofile', require('./routes/api/userprofile'));
+
+
+//routes.initialize(app);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server satrted on port : '+ PORT));
