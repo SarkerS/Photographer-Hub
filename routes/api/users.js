6f00e59d054be871e4e4c97342215a5c2cbc6aa3
Sibendu Sarker <sibendu.sarker@mitt.ca>
2020-01-14 12:35:04 -0600

    User Ragistration - register user to the database

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
 const Users = require('../../models/Users');
+const gravater = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 
 //router.get('/', (req, res) => res.send('User route'));
@@ -14,21 +16,59 @@ router.post('/', [
     check('password', 'Please follow password requirments').isLength({min:6})
 ], 
 
-(req, res) => {
+async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){ // error check
         return res.status(400).json({errors : errors.array()}); // error msg return if exists
     }
 
-    // see if user exists
+    const {name, email, password} = req.body; // extracting info into constant for later use
 
-    // Get user gravatar
+    try {
+        // see if user exists
 
-    // Encrypt Password using bcrypt
+        let user = await Users.findOne({email});
+        if(user){
+             res.status(400).json({errors: [{msg: "This email has already an account"}]});
+        }
 
-    // return jwt for logged in
+        // Get user gravatar
 
- res.send('User route');
+        const avatar = gravater.url(email, { // gravater returns an url associated with email
+            s: '200', // size
+            r: 'pg' , // rating
+            d: 'mm'  //default image
+
+        })
+
+        // create a user instance with all the info: name, avatar, pass, email
+
+        user = new Users({
+            name,
+            email,
+            password,
+            avatar
+        });
+
+        // Encrypt Password using bcrypt
+
+        const salt = await bcrypt.genSalt(10);// create salt
+        user.password = await bcrypt.hash(password, salt); // password hashing with salt
+
+        //Save user to the database
+        await user.save();
+
+        // return jwt for logged in
+
+        res.send('User Registered');
+        
+    } 
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+
+    }
+
 });
 
 
