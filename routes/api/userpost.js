64b42a0f99b20396073dc6c9eaae30532597b5fa
Sibendu Sarker <sibendu.sarker@mitt.ca>
2020-02-12 14:04:09 -0600

    GET route for fetching a single userpost (id = postid, not userid)/ all post

--- routes/api/userpost.js
+++ routes/api/userpost.js
@@ -41,13 +41,10 @@ const upload = multer({
 const {check, validationResult} = require('express-validator');
 
 
-router.get('/', (req, res) => res.send('User post route'));
 
 
 
-
-
-// Private POST request to create or update user profile
+// Private POST request to create user post
 
 router.post('/', upload.single('userimage'), // multer middleware for file uploading 
     [auth, //using middleware
@@ -86,18 +83,44 @@ router.post('/', upload.single('userimage'), // multer middleware for file uploa
             res.status(500).send("Server Error for UserPost");
           }
 
-
-
-
-        
-          
-         
       }
 )
 
 
+// Private GET request to fetch all userpost
+
+router.get('/', auth, async (req, res) => {
+    try {
+      const posts = await Post.find().sort({ date: -1 }); // we want to LIFO manner posts, meaning last post should be fetch first
+      res.json(posts);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error for fetching all UserPost');
+    }
+  });
 
 
+
+// Private GET request to fetch userpost by ID (single post)
+
+router.get('/:user_id', auth, async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.user_id);
+  
+      // Check for ObjectId format and post
+      if (!post) {
+        return res.status(404).json({ msg: 'Post not found' });
+      }
+  
+      res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId') // This is for detecting a special kind of error! User id => "5e29e8a7f17c3814689b5cca" is valid but what if we pass 1 as a user id? it will throw to catch block. therefore we put a msg here also that the user id is not valid
+            return res.status(400).json({msg: 'Post not found'});
+  
+      res.status(500).send('Server Error for fetching UserPost by id');
+    }
+  });
 
 
 
